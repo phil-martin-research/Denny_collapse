4605cb41f52f6341f53bed32f884242937374ae6
Phil Martin <philip.martin.1981@gmail.com>
2014-07-22 11:20:38 +0200

    added analysis of spatial autocorrelation of basal area

--- Scripts/Data_exploration.R
+++ Scripts/Data_exploration.R
@@ -157,4 +157,28 @@ ggplot(data=combined.df,aes(x=lag,y=semi,size=No_pairs))+geom_line(size=0.5)+geo
 
 
 #next look at plot level - BA
+BA_plots_merge<-merge(BA_change_CC,Transect_coord,by="Block")
+head(BA_plots_merge)
+
+
+#now do comparison for each years worth of data
+Years<-unique(BA_plots_merge$Year)
+for (i in 1:length(Years)){
+  Plots_year<-subset(BA_plots_merge,Year==Years[i])
+  dists<-dist(Plots_year[,5:6])
+  breaks<-seq(0,0.01,0.0001)
+  v1 <- variog(coords = Plots_year[,5:6], data = Plots_year[,3], breaks = breaks)
+  v1.summary <- cbind(c(1:length(v1$v)), v1$v, v1$n,Years[i])
+  colnames(v1.summary) <- c("lag", "semi", "No_pairs","Year")
+  setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data/Processing/BA")
+  write.csv(v1.summary,paste("Plot_BA_AC",Years[i],".csv"))
+}
+
+#rbind all years data together
+files  <- list.files(pattern = '\\.csv')
+tables <- lapply(files, read.csv, header = TRUE)
+combined.df <- do.call(rbind , tables)
+
+#plot semi-variogram for all years
+ggplot(data=combined.df,aes(x=lag,y=semi,size=No_pairs))+geom_line(size=0.5)+geom_point(alpha=0.5)+facet_wrap(~Year)
 
