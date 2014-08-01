04cd44c27f0e00fad5420287ade204e34347cd6a
Phil Martin <philip.martin.1981@gmail.com>
2014-08-01 12:08:35 +0200

    added code in an attempt to work out tree positions

--- Scripts/Plot_cleaning.R
+++ Scripts/Plot_cleaning.R
@@ -5,6 +5,12 @@ library(plyr)
 library(reshape2)
 library(geoR)
 library(ape)
+library(ggmap)
+library(sp)
+library(raster)  
+library(rgdal)  
+library(rgeos)  
+library(spdep)
 
 #import data
 setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data")
@@ -12,6 +18,116 @@ setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse
 Coord<-read.csv("Transect_coords.csv")
 #data on trees
 Plots<-read.csv("Denny_plots_edit3.csv")
+
+
+
+
+#first organise data to give plot level information
+head(Plots)
+head(Coord)
+
+#give block names to coordinates
+Coord$Block<-as.numeric(substring(Coord$name, 2))+1
+Coord<-Coord[with(Coord, order(Block)), ]
+#give block names to coordinates
+Plots$Block_new<-Plots$Block_new+1
+Plots<-Plots[with(Plots, order(Block_new)), ]
+
+#make points into a spatial dataframe
+coordinates(Coord)<-~lon+lat
+
+#calculate angle of first transect
+#subset to include enclosed transect only
+Coord_E<-subset(Coord,Transect=="Enclosed")
+#create loop to work out angle between each plot to determine the heading needed 
+
+plot(Coord_E)
+lines(Coord_E$lon,Coord_E$lat)
+abs(Coord_E$lon[1]-Coord_E$lon[1+1])
+
+Angles<-NULL
+for (i in 1:nrow(Coord_E)-1){
+  Lon<-abs(Coord_E$lon[i+1]-Coord_E$lon[i])
+  Lat<-abs(Coord_E$lat[i+1]-Coord_E$lat[i])
+  A<-ifelse(Lon<Lat,asin(Lon/Lat),asin(Lat/Lon))*57.2957795
+  Angles<-rbind(Angles,data.frame(Block=Coord_E$Block[i],Angle=A))
+}
+
+
+x<-(Coord_E$lon[2+1]-Coord_E$lon[2])/(Coord_E$lat[2+1]-Coord_E$lat[2])
+x2<-(10/(R*cos(pi*Coord_E$lat[2]/180)))
+lon0<-Coord_E$lon[2] + (x*(x2 * 180/pi))
+
+
+
+lat1<-Coord_E$lat[1]
+lon1<-Coord_E$lon[1]
+
+#Earth’s radius, sphere
+R<-6378137
+
+#offsets in meters
+dn<-20
+de<-20
+
+#Coordinate offsets in radians
+dLat<-dn/R
+dLon<-de/(R*cos(pi*lat1/180))
+
+#OffsetPosition, decimal degrees
+lat0<-lat1 - dLat * 180/pi
+lon0<-lon1 + dLon * 180/pi 
+
+
+lat0
+
+
+
+#so to work out tree distances I need to use geometry
+head(Plots)
+Plots_E<-subset(Plots,En.Un=="Enclosed")
+
+#run a for loop to calculate the position of individual trees
+Plots_E2<-merge(Plots_E,Angles,by.x="Block_new",by.y="Block")
+for (i in 1:nrow(Plots_E2)){
+  sin(Plots_E2$Angle[1])*(as.numeric(as.character(Plots_E2$Dist_south[1]))/111111)
+}
+Plots_E2$Dist_south[1]
+Plots_E2$Dist_west[1]
+Plots_E2$Angle[1]
+Coord_E$lat[1]
+Coord_E$lon[1]
+Coord_E$lat[2]
+Coord_E$lon[2]
+
+plot(c(Coord_E$lon[1:2],Coord_E$lon[1]),c(Coord_E$lat[1:2],Coord_E$lat[1]-20/100000))
+
+Plot1<-data.frame(Lon=c(Coord_E$lon[1:2],Coord_E$lon[1],Coord_E$lon[2]),Lat=c(Coord_E$lat[1:2],Coord_E$lat[2],Coord_E$lat[1]))
+coordinates(Plot1)<-~Lon+Lat
+
+plot(Plot1)
+
+
+20/111111
+
+
+summary(as.numeric(as.character(Plots_E2$Dist_south)))
+summary(as.numeric(as.character(Plots_E2$Dist_west)))-10
+
+
+
+?rbind()
+
+(atan((max(Coord_E$lon)-min(Coord_E$lon))/(max(Coord_E$lat)-min(Coord_E$lat))))*57.2957795
+
+atan(0.75)
+
+
+#look at plot location
+df<-data.frame(Coord$lon,Coord$lat)
+map<-
+ggmap(get_map(maptype = 'satellite',location = c(mean(Coord$lon),mean(Coord$lat)),zoom=15))+geom_point(data=Coord,aes(x=lon,y=lat,label=Block),size=10,shape=0)
+?get_map
 
 #calculate DBH where needed
 for (i in 1:nrow(Plots)){
