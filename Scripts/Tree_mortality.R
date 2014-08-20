b0feabd70bea0cc3df9f82d556b4fb358a0bb505
Phil Martin <philip.martin.1981@gmail.com>
2014-08-20 10:17:38 +0200

    added glms to work out mortality

--- Scripts/Tree_mortality.R
+++ Scripts/Tree_mortality.R
@@ -14,6 +14,9 @@ library(gridExtra)
 library(MASS)
 library(survival)
 library(GGally)
+library(lme4)
+library(fields)
+library(ROCR)
 
 #import data
 setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data")
@@ -28,20 +31,75 @@ DBH<-subset(DBH,In_out=="In")
 
 #first some exploratory analysis to look at variation in tree deaths by year
 #subset the data to only include data from 1964
-DBH_64<-subset(DBH,Year==1964|1988|1984)
+DBH_64<-subset(DBH,Year<1989&Year>1960)
+head(DBH_64)
 ggplot(DBH,aes(x=as.factor(Status),y=DBH))+geom_boxplot()+facet_wrap(~Year)
 
 
-#use function to work out the base rate proabability of mortality for each period
-head(DBH_64)
-Tree_ID<-unique(DBH_64$Tree_ID)
-Mort<-NULL
-for (i in 1:unique(Tree_ID)){
-  for (y in 1:nrow(DBH_64)){
- Stat<-ifelse(DBH_64$Tree_ID[y]==Tree_ID[i]&DBH_64$Year[y]==1988|1984,DBH_64$Status[y],NA)
- Mort<-rbind(Mort,Stat)
+#toy example for data from 1964 to 1984/88
+Alive_64<-subset(DBH_64,Status==1&Year==1964)[,3]
+#create a loop to only give those trees which were alive in 1964 and their status in 1984/88
+Mort_88<-NULL
+for (i in 1:length(Alive_64)){
+Mort<-subset(DBH_64,Tree_ID==Alive_64[i]&Year==1988)
+Mort_88<-rbind(Mort_88,Mort)
 }
+head(Mort_88,n = 20)
+
+#change status to 1=dead, 0=alive
+Mort_88$Dead<-ifelse(Mort_88$Status==1,0,1)
+Mort_88_2<-Mort_88[complete.cases(Mort_88[,12]),]
+summary(Mort_88_2)
+#work out distance to nearest dead tree
+Distances<-rdist(Mort_88_2[9:10])
+Mort_88_2$Tree_ID
+arrayInd(4, dim(Distances))
+
+#produce function to remove distances for live trees
+for (i in 1:nrow(Distances)){
+  for (y in 1:ncol(Distances)){
+    Distances[i,y]<-ifelse(Mort_88_2$Dead[i]==1,Distances[i,y],NA)
+  }
 }
+Distances[Distances==0]<-NA
+
+#now work out minimum distance to a dead tree
+for (i in 1:ncol(Distances)){
+  Mort_88_2$Dead_Dist[i]<-min(Distances[,i],na.rm = T)
+}
+summary(Mort_88_2$Dead_Dist)
+
+#now work out DBH of nearest dead tree
+for (i in 1:ncol(Distances)){
+  Index<-match(min(Distances[,i],na.rm = T),Distances[,i])
+  Mort_88_2$Dead_DBH[i]<-Mort_88_2$DBH[Index]
+}
+
+#model of mortality from 1964-1988
+M0<-glm(Dead~1,data=Mort_88_2,family=binomial(link = "logit"))
+M1<-glm(Dead~DBH,data=Mort_88_2,family=binomial(link = "logit"))
+M2<-glm(Dead~DBH+I(DBH^2),data=Mort_88_2,family=binomial(link = "logit"))
+M3<-glm(Dead~DBH+Dead_Dist,data=Mort_88_2,family=binomial(link = "logit"))
+M4<-glm(Dead~Dead_Dist,data=Mort_88_2,family=binomial(link = "logit"))
+M5<-glm(Dead~Dead_Dist+Dead_DBH,data=Mort_88_2,family=binomial(link = "logit"))
+M6<-glm(Dead~Dead_Dist*Dead_DBH,data=Mort_88_2,family=binomial(link = "logit"))
+M7<-glm(Dead~DBH+Species,data=Mort_88_2,family=binomial(link = "logit"))
+M8<-glm(Dead~DBH*Species,data=Mort_88_2,family=binomial(link = "logit"))
+
+
+
+plot(Mort_88_2$Dead_Dist,(plogis(predict(M4)))/24)
+plot(Mort_88_2$DBH,(plogis(predict(M1)))/24)
+plot(Mort_88_2$DBH,Mort_88_2$Dead)
+
+auc.tmp <- performance(predict(M1),"auc")
+auc <- as.numeric(auc.tmp@y.values)
+plot(M1)
+
+################################################################################
+#below this point is the survival analysis code, not sure that this is much use#
+################################################################################
+
 
 #include only complete cases - those with data on whether stems are alive or dead and not nas
 DBH<-DBH[complete.cases(DBH[,6]),]
