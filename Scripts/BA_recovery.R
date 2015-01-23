41a897991ebf2cdc5981915592b604a2429b28ae
Phil Martin <philip.martin.1981@gmail.com>
2015-01-23 12:49:57 +0100

    added code to analyse relationships between basal area, time since collapse and intitial loss of basal area

--- Scripts/BA_recovery.R
+++ Scripts/BA_recovery.R
@@ -7,6 +7,8 @@ library(ggplot2)
 library(plyr)
 library(reshape2)
 library(gridExtra)
+library(lme4)
+library(MuMIn)
 
 #load in data
 setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data")
@@ -25,24 +27,50 @@ Plots2<-NULL
 Block_unique<-unique(BA$Block)
 for (i in 1:length(Block_unique)){
   Block_sub<-subset(BA,Block==Block_unique[i])
-  Block_sub$Collapse4<-NA
-  Block_sub$Collapse4[1]<-0
-  Block_sub$Time_Coll<-NA
-  Block_sub$Coll_Sever<-NA
+  Block_sub$Collapse2<-NA
+  Block_sub$Collapse2[1]<-0
   for (y in 2:nrow(Block_sub)){  
-    if (Block_sub$Collapse4[y-1]==0&Block_sub$Collapse[y]==0) {
-    Block_sub$Collapse4[y]<-0
-    }else{Block_sub$Collapse4[y]<-1    
+    if (Block_sub$Collapse2[y-1]==0&Block_sub$Collapse[y]==0) {
+    Block_sub$Collapse2[y]<-0
+    }else{Block_sub$Collapse2[y]<-1    
     }
   }
-  Block_sub$Collapse2<-ifelse(sum(Block_sub$Collapse)>0,1,0)
-  Block_sub$Collapse3<-ifelse(sum(Block_sub$Collapse2)>0&tail(Block_sub$Collapse,1)==0,2,Block_sub$Collapse2)
-  Plots2<-rbind(Block_sub,Plots2)
+  Block_sub2<-subset(Block_sub,Collapse2==1)
+  if(nrow(Block_sub2)>0){
+  Block_sub2$Time_Coll<-Block_sub2$Year-Block_sub2$Year[1]
+  Block_sub2$Coll_Sever<-Block_sub2$BAPERCM[1]
+  Plots2<-rbind(Block_sub2,Plots2)
+  }
 }
 
 
-ggplot(Plots2,aes(x=Year,y=BAPERCM,group=Block))+geom_point()+geom_line()+facet_wrap(~Collapse3)+geom_smooth(se=F,method="lm",colour="blue",size=3,aes(group=NULL))
+head(Plots3)
+
+ggplot(Plots3,aes(x=Coll_Sever,y=BAPERCM,group=Block))+geom_point()+geom_line()+facet_wrap(~Block)
 
 
 #now subset to give plots post decline
 Plots3<-subset(Plots2,Collapse3==1)
+Plots3<-subset(Plots2,Time_Coll>0)
+
+#model of recovery as a function of collapse
+M0.1<-lmer(BAPERCM~(1|Block),data=Plots3)
+M0.2<-lmer(BAPERCM~(0+Block|Time_Coll),data=Plots3)
+M0.3<-lmer(BAPERCM~(1|Block)+(0+Block|Time_Coll),data=Plots3)
+M0.3<-lmer(BAPERCM~(1|Block)+(0+Block|Coll_Sever),data=Plots3)
+
+AICc(M0.1,M0.2)
+
+#go with M0.1 random effects
+M1<-lmer(BAPERCM~Time_Coll*Coll_Sever+(1|Block),data=Plots3)
+M2<-lmer(BAPERCM~Time_Coll+Coll_Sever+(1|Block),data=Plots3)
+M3<-lmer(BAPERCM~Time_Coll+(1|Block),data=Plots3)
+M4<-lmer(BAPERCM~Coll_Sever+(1|Block),data=Plots3)
+
+AICc(M1,M2,M3,M4)
+
+plot(Plots3$Coll_Sever,Plots3$BAPERCM)
+points(Plots3$Coll_Sever,predict(M2,re.form = NA),col="red")
+abline(a=0,b=1)
+plot(Plots3$Time_Coll,Plots3$BAPERCM)
+points(Plots3$Time_Coll,predict(M2,re.form = NA),col="red")
