414491cf870586df5238c401d50a8978d949410a
Phil Martin <philip.martin.1981@gmail.com>
2015-01-26 17:30:35 +0100

    added new plots of trait change over collapse gradient

--- Scripts/GF_traits_TS_Grad.R
+++ Scripts/GF_traits_TS_Grad.R
@@ -28,15 +28,28 @@ for (i in 1:length(Block_unique)){
 
 #exploratory plots of change in different traits over time
 #light
-ggplot(GF2,aes(x=Year,y=Light,group=Block))+geom_point()+geom_line()+facet_wrap(~Collapse2)+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
+Light_plot<-ggplot(GF2,aes(x=Year,y=Light,group=Block))+geom_point()+facet_wrap(~Collapse2)+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
+Light_plot2<-Light_plot+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))+ylim(c(4,7))
+Light_plot2+ylab("Community weighted mean plant light requirements")
+setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures")
+ggsave("Collapse_Light_TS.png",width = 8,height=6,units = "in",dpi=300)
+
 #Nitrogen
-ggplot(GF2,aes(x=Year,y=Nit,group=Block))+geom_point()+geom_line()+facet_wrap(~Collapse2)+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
+Nitrogen_plot<-ggplot(GF2,aes(x=Year,y=Nit,group=Block))+geom_point()+facet_wrap(~Collapse2)+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
+Nitrogen_plot2<-Nitrogen_plot+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))+ylim(c(2,7))
+Nitrogen_plot2+ylab("Community weighted mean plant nitrogen requirements")
+setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures")
+ggsave("Collapse_Nitrogen_TS.png",width = 8,height=6,units = "in",dpi=300)
 #Moisture
-ggplot(GF2,aes(x=Year,y=Moist,group=Block))+geom_point()+geom_line()+facet_wrap(~Collapse2)+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
+Moisture_plot<-ggplot(GF2,aes(x=Year,y=Moist,group=Block))+geom_point()+facet_wrap(~Collapse2)+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
+Moisture_plot2<-Moisture_plot+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))+ylim(c(4,8))
+Moisture_plot2+ylab("Community weighted mean plant water requirements")
+setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures")
+ggsave("Collapse_Moisture_TS.png",width = 8,height=6,units = "in",dpi=300)
 
 #exploratory plots of change in different traits over the gradient
 #light
-ggplot(GF2,aes(x=BAPERCM,y=Light_Change,group=Block))+geom_point()+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
+ggplot(GF2,aes(x=BAPERCM,y=Light_Change,group=Block))+geom_point()+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))+ylim(c(-2,4))
 #Nitrogen
 ggplot(GF2,aes(x=BAPERCM,y=Nit_Change,group=Block))+geom_point()+geom_smooth(se=F,method="lm",colour="blue",size=2,aes(group=NULL))
 #Moisture
