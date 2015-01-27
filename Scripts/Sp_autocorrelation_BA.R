d8a0e281a3ed621928bdabd951ba1632c7f1f7c6
Phil Martin <philip.martin.1981@gmail.com>
2015-01-27 11:51:26 +0100

    removed bits of script that are no longer useful

--- Scripts/Sp_autocorrelation_BA.R
+++ Scripts/Sp_autocorrelation_BA.R
@@ -11,8 +11,16 @@ library(ncf)
 setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data")
 BA<-read.csv("BA_Gradient.csv")
 
+
+
+BA$Year<-ifelse(BA$Year==1996,1999,BA$Year)
+BA$Transect<-ifelse(BA$Block>51,"Unenclosed","Enclosed")
+
+
 #test for autocorrelation in the data
 YU<-unique(BA$Year)
+
+unique(BA[c("Year", "Transect")])
 Morans_stat<-NULL
 for (i in 2:length(YU)){
   BA_sub<-subset(BA,Year==YU[i])
@@ -26,83 +34,32 @@ for (i in 2:length(YU)){
   Morans_stat<-rbind(Moran_sub_df,Morans_stat)
 }
 
-data.frame(Moran.I(BA_2014[,4], Dist_mat_Inv))
-
-Dist_mat<-as.matrix(dist(cbind(BA_2014$Northing, BA_2014$Easting)))
-Dist_mat_Inv<-1/Dist_mat
-Dist_mat_Inv[is.infinite(Dist_mat_Inv)] <- 0
-diag(Dist_mat_Inv) <- 0
-Moran.I(BA_2014[,4], Dist_mat_Inv)
-
-plot(BA_2014[,6:5])
-
-dists<-dist(BA_2014[,6:5])
-breaks<-seq(0,1100,10)
-v1 <- variog(coords = BA_2014[,6:5], data = BA_2014[,4], breaks = breaks)
-v1.summary <- cbind(c(1:length(v1$v)), v1$v, v1$n)
-colnames(v1.summary) <- c("lag", "semi", "No_pairs")
-v1.summary<-data.frame(v1.summary)
-
-ggplot(v1.summary,aes(x=lag*10,y=semi,size=No_pairs))+geom_point()+geom_line(aes(size=NULL))+geom_smooth(method="lm",se=F,size=2)
 
 #look at variation in morans I over distance
-ncf.cor <- correlog(BA_2014$Easting, BA_2014$Northing, BA_2014$BAPERCM,
-                    increment=25, resamp=1000)
+Spatial_correlation<-NULL
+YU<-unique(BA[c("Year", "Transect")])
+YU<-subset(YU,Year>1964)
+head(YU)
+for (i in 1:nrow(YU)){
+  BA_sub<-subset(BA,Year==YU[i,1])
+  BA_sub<-subset(BA_sub,Transect==YU[i,2])
+  ncf.cor <- correlog(BA_sub$Easting, BA_sub$Northing, BA_sub$BAPERCM,
+                      increment=25, resamp=1000)
+  Spatial_correlation_sub<-data.frame(Dist=ncf.cor$mean.of.class,MoranI=ncf.cor$correlation,Pval=ncf.cor$p,Number=ncf.cor$n)
+  Spatial_correlation_sub$Sig<-ifelse(Spatial_correlation_sub$Pval<0.05,"S","NS")
+  Spatial_correlation_sub$Year<-YU[i,1]
+  Spatial_correlation_sub$Transect<-YU[i,2]
+  Spatial_correlation<-rbind(Spatial_correlation,Spatial_correlation_sub)
+}
 
-Spatial_correlation<-data.frame(Dist=ncf.cor$mean.of.class,MoranI=ncf.cor$correlation,Pval=ncf.cor$p,Number=ncf.cor$n)
-Spatial_correlation$Sig<-ifelse(Spatial_correlation$Pval<0.05,"S","NS")
+#subset to give only comparisons with >15 pairs
+Spatial_correlation_sub<-subset(Spatial_correlation,Number>15)
 
 #plot how morans I changes over distance
 theme_set(theme_bw(base_size=12))
-Morans_I_plot1<-ggplot(Spatial_correlation,aes(x=Dist,y=MoranI,size=Number,colour=Sig))+geom_point()+geom_line(aes(size=NULL,group=NULL,colour=NULL))+geom_hline(y=0,lty=2)
+Morans_I_plot1<-ggplot(Spatial_correlation,aes(x=Dist,y=MoranI,size=Number,colour=Sig))+geom_point()+geom_line(aes(size=NULL,group=NULL,colour=NULL))+geom_hline(y=0,lty=2)+facet_grid(Transect~Year)
 Morans_I_plot2<-Morans_I_plot1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
-Morans_I_plot2+ylab("Moran's I")+xlab("Distance between plots (m)")+theme(legend.position="none")
+Morans_I_plot2+ylab("Moran's I")+xlab("Distance between plots (m)")+theme(legend.position="none")+scale_size_continuous(range = c(1, 3))
+setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures")
+ggsave("MoransI_distance.png",width = 8,height=6,units = "in",dpi=300)
 
-
-#now do comparison for each years worth of data
-Years<-unique(SD_plots_merge$Year)
-for (i in 1:length(Years)){
-  Plots_year<-subset(SD_plots_merge,Year==Years[i])
-  dists<-dist(Plots_year[,5:6])
-  breaks<-seq(0,0.01,0.0001)
-  v1 <- variog(coords = Plots_year[,5:6], data = Plots_year[,3], breaks = breaks)
-  v1.summary <- cbind(c(1:length(v1$v)), v1$v, v1$n,Years[i])
-  colnames(v1.summary) <- c("lag", "semi", "No_pairs","Year")
-  setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data/Processing/SD")
-  write.csv(v1.summary,paste("Plot_SD_AC",Years[i],".csv"))
-}
-
-#rbind all years data together
-files  <- list.files(pattern = '\\.csv')
-tables <- lapply(files, read.csv, header = TRUE)
-combined.df <- do.call(rbind , tables)
-
-#plot semi-variogram for all years
-ggplot(data=combined.df,aes(x=lag,y=semi,size=No_pairs))+geom_line(size=0.5)+geom_point(alpha=0.5)+facet_wrap(~Year)
-
-
-#next look at plot level - BA
-BA_plots_merge<-merge(BA_change_CC,Transect_coord,by="Block")
-head(BA_plots_merge)
-
-
-#now do comparison for each years worth of data
-Years<-unique(BA_plots_merge$Year)
-for (i in 1:length(Years)){
-  Plots_year<-subset(BA_plots_merge,Year==Years[i])
-  dists<-dist(Plots_year[,5:6])
-  breaks<-seq(0,0.01,0.0001)
-  v1 <- variog(coords = Plots_year[,5:6], data = Plots_year[,3], breaks = breaks)
-  v1.summary <- cbind(c(1:length(v1$v)), v1$v, v1$n,Years[i])
-  colnames(v1.summary) <- c("lag", "semi", "No_pairs","Year")
-  setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data/Processing/BA")
-  write.csv(v1.summary,paste("Plot_BA_AC",Years[i],".csv"))
-}
-
-#rbind all years data together
-files  <- list.files(pattern = '\\.csv')
-tables <- lapply(files, read.csv, header = TRUE)
-combined.df <- do.call(rbind , tables)
-
-#plot semi-variogram for all years
-ggplot(data=combined.df,aes(x=lag,y=semi,size=No_pairs))+geom_line(size=0.5)+geom_point(alpha=0.5)+facet_wrap(~Year)
