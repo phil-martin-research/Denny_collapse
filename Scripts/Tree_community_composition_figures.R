eca8c6124e51dba6e80735609be1c7d528921a2e
Phil Martin <philip.martin.1981@gmail.com>
2015-02-10 16:06:56 +0100

    edited figures for community change

--- Scripts/Tree_community_composition_figures.R
+++ Scripts/Tree_community_composition_figures.R
@@ -35,12 +35,38 @@ r.squaredGLMM(M2)
 Plots2$Pred<-plogis(predict(M2,re.form=NA))
 Plots3<-subset(Plots2,BAPERCM<1)
 
+newdat<-expand.grid(BAPERCM=seq(min(Plots2$BAPERCM),0.5,0.001))
+newdat$Tanner2<-predict(M2,re.form=NA,newdata=newdat)
+mm <- model.matrix(terms(M2),newdat)
+newdat$distance <- mm %*% fixef(M2)
+pvar1 <- diag(mm %*% tcrossprod(vcov(M2),mm))
+tvar1 <- pvar1+VarCorr(M2)$Year[1]  ## must be adapted for more complex models
+newdat <- data.frame(
+  newdat
+  , plo = newdat$distance-2*sqrt(pvar1)
+  , phi = newdat$distance+2*sqrt(pvar1)
+  , tlo = newdat$distance-2*sqrt(tvar1)
+  , thi = newdat$distance+2*sqrt(tvar1)
+)
 
+
+newdat$Pred<-plogis(predict(M2,newdata = newdat,re.form=NA))
+newdat$plo2<-plogis(newdat$plo)
+newdat$phi2<-plogis(newdat$phi)
+
+#identify plots on unenclosed and enclosed transects
+Plots3$Transect<-ifelse(Plots3$Block>=51,"Unenclosed","Enclosed")
+
+nrow(Plots3)
 
 #plot this
 theme_set(theme_bw(base_size=12))
-Coll_Tanner1<-ggplot(Plots3,aes(x=BAPERCM*100*(-1),y=Tanner,colour=as.factor(Year)))+geom_point(shape=1)
+Coll_Tanner1<-ggplot(Plots3,aes(x=BAPERCM*100*(-1),y=Tanner,colour=as.factor(Year),shape=Transect))+geom_point(size=3)
 Coll_Tanner2<-Coll_Tanner1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
-Coll_Tanner3<-Coll_Tanner2+geom_line(data=Plots3,aes(x=BAPERCM*100*(-1),y=Pred,colour=NULL))+ylab("Tanner similarity to same plot in 1964")+xlab("Percentage basal area loss since 1964")
-Coll_Tanner3+scale_colour_brewer(palette="Set1","Year")
+Coll_Tanner3<-Coll_Tanner2+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=Pred,shape=NULL),colour="black")+ylab("Tanner index")+xlab("Percentage basal area loss since 1964")
+Coll_Tanner4<-Coll_Tanner3+ scale_colour_grey("Year of survey")+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=plo2,shape=NULL),colour="black",lty=2)+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=phi2,shape=NULL),colour="black",lty=2)
+Coll_Tanner4+scale_shape_manual(values=c(16,1))
 ggsave("Figures/BA_Tanner.png",width = 8,height=6,units = "in",dpi=300)
+Coll_Tanner4<-Coll_Tanner3+ scale_colour_brewer(palette = "Set1","Year of survey")+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=plo2,shape=NULL),colour="black",lty=2)+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=phi2,shape=NULL),colour="black",lty=2)
+Coll_Tanner4+scale_shape_manual(values=c(16,1))
+ggsave("Figures/BA_Tanner_colour.png",width = 8,height=6,units = "in",dpi=300)
