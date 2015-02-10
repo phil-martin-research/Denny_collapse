67d09a3c8304c013fb675bbb2178ac51f52e6ca8
Phil Martin <philip.martin.1981@gmail.com>
2015-02-10 16:04:10 +0100

    edited map to include scale and letters for diferent plots for paper

--- Scripts/Collapse_map.R
+++ Scripts/Collapse_map.R
@@ -21,12 +21,12 @@ for (i in 1:nrow(Plots)){
 #classify plots by the degree to which they have collapsed
 #1- stable or increase, 2 - 0-25% decrease, 3 - 25-50% decrease, 
 #4 - 50-75% decrease, 5 75-100% decrease
-Groups<-data.frame(max=c(-0.75,-0.50,-0.25,0,3),min=c(-1.00,-0.75,-0.50,-0.25,0),group=c(5,4,3,2,1))
-Plots$Collapse_group<-NA
+Groups<-data.frame(max=c(-0.75,-0.50,-0.25,0,3),min=c(-1.00,-0.75,-0.50,-0.25,0),group=as.character(c("Stable or increase","0-25% loss","25-50% loss","50-75% loss","75-100% loss")))
+Plots$Collapse_group<-"Empty"
 for (i in 1:nrow(Groups)){
   for (y in 1:nrow(Plots)){
-  Plots$Collapse_group[y]<-ifelse(Plots$BAPERCM[y]<Groups[i,1]&Plots$BAPERCM[y]>Groups[i,2],Groups[i,3],Plots$Collapse_group[y])
-  Plots$Collapse_group[y]<-ifelse(Plots$Year[y]==1964,1,Plots$Collapse_group[y])
+    Plots$Collapse_group[y]<-as.character(ifelse(Plots$Year[y]==1964,"Stable or increase",Plots$Collapse_group[y]))
+    Plots$Collapse_group[y]<-as.character(ifelse(Plots$BAPERCM[y]<Groups[i,1]&Plots$BAPERCM[y]>Groups[i,2],as.character(Groups$group[i]),Plots$Collapse_group[y]))
   }
 }
 
@@ -37,8 +37,19 @@ for (i in 1:nrow(Plots)){
   Plots$Year[i]<-ifelse(Plots$Year[i]==1996,1999,Plots$Year[i])
 }
 
+#change 1996 to 1999
+for (i in 1:nrow(Plots)){
+  Plots$Year[i]<-ifelse(Plots$Year[i]==1999,"1996/9",Plots$Year[i])
+}
 
-#set all plots in 1964 as uncollapsed (1)
+#reorder factor for collapse groups to plot from stable to most collapsed
+Plots$Collapse_group <- factor(Plots$Collapse_group, c("Stable or increase","0-25% loss","25-50% loss","50-75% loss","75-100% loss"))
+
+#add labels for text annotations
+labels<-data.frame(x=min(Plots$Easting)+10,y=max(Plots$Northing),Year=c("1964","1984","1988","1996/9","2014"),labs=c("(a)","(b)","(c)","(d)","(e)"))
+  
+#add scale bar
+scale<-data.frame(x=max(Plots$Easting)-150,y=max(Plots$Northing)-250,Year=c("1964"))
 
 
 
@@ -63,8 +74,12 @@ Collapse_map3<-Collapse_map2+scale_colour_grey("Collapse group",start=0.8,end=0.
   axis.text.y=element_blank(),axis.ticks=element_blank(),
   axis.title.x=element_blank(),
   axis.title.y=element_blank())
-Collapse_map3+coord_fixed(ratio = 1)+xlim(c(432950,433450))
+Collapse_map4<-Collapse_map3+coord_fixed(ratio = 1)+xlim(c(432930,433450))+ geom_text(data=labels,aes(x, y, label=labs, group=NULL),colour="black")
+Collapse_map4+geom_segment(data=scale,aes(x = x, y = y, xend = x+200, yend = y),colour="black")+geom_text(data=scale,aes(x+90, y+50, label="200m", group=NULL),colour="black")
 ggsave("Figures/Collapse_map_groups.png",width = 8,height=6,units = "in",dpi=300)
+Collapse_map3+coord_fixed(ratio = 1)+xlim(c(432930,433450))+ geom_text(data=labels,aes(x, y, label=labs, group=NULL),colour="black")+ 
+  theme(strip.background = element_blank(), strip.text = element_blank())+geom_segment(data=scale,aes(x = x, y = y, xend = x+200, yend = y),colour="black")+geom_text(data=scale,aes(x+90, y+50, label="200m", group=NULL),colour="black")
+ggsave("Figures/Collapse_map_nolab.png",width = 8,height=6,units = "in",dpi=300)
 
 #plot location of plots using continous variable
 theme_set(theme_bw(base_size=12))
