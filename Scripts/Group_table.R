331d052591ef4bd5c5272c59a8a3abdc6d8bb27c
Phil Martin <philip.martin.1981@gmail.com>
2015-02-13 12:15:54 +0100

    changed totally how groups are calculated for woody species

--- Scripts/Group_table.R
+++ Scripts/Group_table.R
@@ -10,7 +10,8 @@ library(gridExtra)
 
 #load in data
 setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data")
-BA<-read.csv("Denny_plots.csv")
+BA<-read.csv("Data/Denny_plots.csv")
+BA$Open_transect<-ifelse(BA$Block>51,1,0)
 
 #calculate Tanner index the mean of Sorensen weighted by BA and SD
 BA$Tanner<-(BA$Sor_BA+BA$SorM)/2
@@ -30,48 +31,33 @@ for(i in 1:nrow(Groups)){
 
 head(BA_groups)
 
+head(BA_groups)
+
 #now do calculations of means for year and group
 #first drop the colums I don't need
 #I want to keep BA, species richness,sorenson, etc
-keeps<-c("Year","Group","BAM","BAPERCM","SDM","Sor_BA","SorM","Tanner","SPRM","LightM","NitM","MoistM")
+keeps<-c("Year","Group","BAM","BAPERCM","SDM","Sor_BA","SorM","Tanner","SPRM","LightM","NitM","MoistM","Open_transect")
 BA_groups2<-BA_groups[keeps]
-df_melt <- melt(BA_groups2, id = c("Year", "Group")) # then melt by year and group
-df_melt$Year[df_melt$Year==1999]<-1996#change 1999 to 1996 becuase only one transect was surveyed in this period
+BA_groups2$Year[BA_groups2$Year==1999]<-1996
 
-#and then cast the data to produce means, SE and the number of plots in each group
-Groups2_Mean<-dcast(df_melt, Year + Group ~ variable, mean,na.rm = TRUE)
-Groups2_SE<-dcast(df_melt, Year + Group ~ variable, std)
-#number of blocks in each group
-Count_plots<-count(BA_groups,c("Year","Group"))
-#merge the dataframes together
-Group_summ<-merge(Groups2_Mean,Groups2_SE,by=c("Year","Group"))
-Group_summ2<-merge(Group_summ,Count_plots,by=c("Year","Group"))
+head(BA_groups2)
+Group_summ<-ddply(BA_groups2,.(Year,Group),summarize,
+      MEAN_BA=paste(round(mean(BAM),2)," (",round(std(BAM),2),")",sep =""),
+      MEAN_BAPERC=paste(round(mean(BAPERCM*100),2)," (",round(std(BAPERCM*100),2),")",sep =""),
+      SDM=paste(round(mean(SDM),2)," (",round(std(SDM),2),")",sep =""),
+      Tanner=paste(round(mean(Tanner),2)," (",round(std(Tanner),2),")",sep =""),
+      SPRM=paste(round(mean(SPRM),2)," (",round(std(SPRM),2),")",sep =""),
+      LightM=paste(round(mean(LightM,na.rm = TRUE),2)," (",round(std(LightM),2),")",sep =""),
+      NitM=paste(round(mean(NitM,na.rm = TRUE),2)," (",round(std(NitM),2),")",sep =""),
+      No=length(BAPERCM),
+      Number_open=sum(Open_transect))
 
 #rename columns
-colnames(Group_summ2)<-c("Year","Group","BA_mean","BA_perc_mean","SD_mean","Sorensen_BA_mean","Sorensen_SD_mean","Tanner_mean","Spr_Mean","Light_mean","Nit_mean","Moist_mean",
-                         "BA_SE","BA_perc_SE","SD_SE","Sorensen_BA_SE","Sorensen_SD_SE","Tanner_SE","Spr_SE","Light_SE","Nit_SE","Moist_SE","no_of_plots")
-#run a loop to work out the proportion of plots in each period found in each group 
-Plot_count<-data.frame(tapply(Group_summ2$no_of_plots,Group_summ2$Year,sum))
-colnames(Plot_count)<-"Count"
-Plot_count$Year<-as.numeric(rownames(Plot_count))
-Group_summ3<-NULL
-for (i in 1:nrow(Plot_count)){
-  Group_plot<-subset(Group_summ2,Year==Plot_count[i,2])
-  Group_plot$Prop_plot<-Group_plot$no_of_plots/Plot_count[i,1]
-  Group_summ3<-rbind(Group_summ3,Group_plot)
-}
-#multiply proportions to give percentage
-Group_summ3$BA_perc_mean<-Group_summ3$BA_perc_mean*100
-Group_summ3$BA_perc_SE<-Group_summ3$BA_perc_SE*100
+colnames(Group_summ)<-c("Year","Group","BA","BA_perc","SD","Tanner","Spr","Light","Nit","no_of_plots","No_open")
 
-#round numeric columns to two decimal places
-is.num <- sapply(Group_summ3, is.numeric)
-Group_summ3[is.num] <- lapply(Group_summ3[is.num], round, 2)
-head(Group_summ3)
-
-#write this csv 
+#write this to a csv 
 setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures")
-write.csv(Group_summ3,file="Group_table.csv",row.names=F)
+write.csv(Group_summ,file="Figures/Group_table.csv",row.names=F)
 
 ###########################################################################
 #transitions table#########################################################
