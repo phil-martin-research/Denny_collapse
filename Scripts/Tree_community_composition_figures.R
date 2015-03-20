52b099d50d6ad7501b5015b4c87dedf1e77f683f
Phil Martin <philip.martin.1981@gmail.com>
2015-03-20 13:36:22 +0000

    edited figure for the tanner index

--- Scripts/Tree_community_composition_figures.R
+++ Scripts/Tree_community_composition_figures.R
@@ -19,20 +19,39 @@ Plots2<-subset(Plots2,Year>1964)
 
 #create variable for tanner that is bounded above 0
 Plots2$Tanner2<-qlogis(Plots2$Tanner)
+Plots2$Year<-ifelse(Plots2$Year==1999,1996,Plots2$Year)
+Plots2$Year2<-ifelse(Plots2$Year==1996,"1996/9",Plots2$Year)
+
+#create plots of changes in tanner index over gradient for different years
+ggplot(Plots2,aes(x=BAPERCM,y=Tanner))+geom_point()+facet_wrap(~Year)+geom_smooth(method="lm")
 
 M0.1<-lmer(Tanner2~1+(1|Block),data=Plots2)
-M0.2<-lmer(Tanner2~1+(Block|Year),data=Plots2)
-AICc(M0.1,M0.2)
-#use first random effect specification
-M1<-lmer(Tanner2~BAPERCM+(Block|Year),data=Plots2)
-M2<-lmer(Tanner2~BAPERCM+I(BAPERCM^2)+(Block|Year),data=Plots2)
-AICc(M1,M2,M0.1)
+M0.2<-lmer(Tanner2~1+(1|Block)+(1|Year),data=Plots2)
+M0.3<-lmer(Tanner2~1+(1|Block)+(BAPERCM|Year),data=Plots2)
+AICc(M0.1,M0.2,M0.3)
+##use first random effect specification
+M1<-lmer(Tanner2~BAPERCM+(1|Block)+(BAPERCM|Year),data=Plots2)
+M2<-lmer(Tanner2~BAPERCM+I(BAPERCM^2)+(1|Block)+(BAPERCM|Year),data=Plots2)
+AICc(M1,M2,M0.3)
 
-plot(plogis(fitted(M2)),resid(M2))
-r.squaredGLMM(M2)
+plot(M2)
+qqnorm(resid(M2))
+
+#create model selection output for these models
+Mod_list<-list(M0.3,M1,M2)
+Model_selection<-model.sel(Mod_list)
+Model_selection$R2<-c(r.squaredGLMM(M2)[1],r.squaredGLMM(M1)[1],r.squaredGLMM(M0.3)[1])
+write.csv(Model_selection,"Figures/Mod_sel_Grad_Tanner.csv")
+
+#get coefficient of top model
+coefs <- data.frame(coef(summary(M2)))
+# use normal distribution to approximate p-value
+coefs$p.z <- 2 * (1 - pnorm(abs(coefs$t.value)))
+coefs
+write.csv(coefs,"Figures/Tanner_Grad_coefs.csv")
 
 #put prediction into dataframe
-Plots2$Pred<-plogis(predict(M2,re.form=NA))
+Plots2$Pred<-plogis(predict(M2))
 Plots3<-subset(Plots2,BAPERCM<1)
 
 newdat<-expand.grid(BAPERCM=seq(min(Plots2$BAPERCM),0.5,0.001))
@@ -57,11 +76,9 @@ newdat$phi2<-plogis(newdat$phi)
 #identify plots on unenclosed and enclosed transects
 Plots3$Transect<-ifelse(Plots3$Block>=51,"Unenclosed","Enclosed")
 
-nrow(Plots3)
-
 #plot this
 theme_set(theme_bw(base_size=12))
-Coll_Tanner1<-ggplot(Plots3,aes(x=BAPERCM*100*(-1),y=Tanner,colour=as.factor(Year),shape=Transect))+geom_point(size=3)
+Coll_Tanner1<-ggplot(Plots3,aes(x=BAPERCM*100*(-1),y=Tanner,colour=as.factor(Year2),shape=Transect))+geom_point(size=3,alpha=0.5)
 Coll_Tanner2<-Coll_Tanner1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
 Coll_Tanner3<-Coll_Tanner2+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=Pred,shape=NULL),colour="black")+ylab("Tanner index")+xlab("Percentage basal area loss since 1964")
 Coll_Tanner4<-Coll_Tanner3+ scale_colour_grey("Year of survey")+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=plo2,shape=NULL),colour="black",lty=2)+geom_line(data=newdat,aes(x=BAPERCM*100*(-1),y=phi2,shape=NULL),colour="black",lty=2)
