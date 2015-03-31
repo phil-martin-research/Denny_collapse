edfe26bec4f4d75bac4fc4dbf80c51b4d0e39b7f
Phil Martin <philip.martin.1981@gmail.com>
2015-03-31 10:55:23 +0200

    modified tree trait models and plots

--- Scripts/Tree_traits.R
+++ Scripts/Tree_traits.R
@@ -19,50 +19,88 @@ library(fields)
 library(ROCR)
 
 #import data
-setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Data")
-BA<-read.csv("Denny_plots.csv")
-
+BA<-read.csv("Data/Denny_plots.csv")
+BA<-subset(BA,Year>1964)
 
 #####################################
 #exploration of trait change#########
 #####################################
 str(BA)
+BA$LightM<-ifelse(BA$LightM==0,NA,BA$LightM)
+BA$NitM<-ifelse(BA$NitM==0,NA,BA$NitM)
+BA$MoistM<-ifelse(BA$MoistM==0,NA,BA$MoistM)
+
+####################################
+#test for changes in traits over####
+#the gradient#######################
+BA$BAPERCM2<-BA$BAPERCM*-1
 
 #first light requirements
-M0.1<-lme(LightM~1,random=~1|Block,data=BA,na.action=na.omit)
-M0.2<-lme(LightM~1,random=~BAPERCM|Block,data=BA,na.action=na.omit)
-AICc(M0.1,M0.2)
+M0<-lmer(LightM~1+(1|Block),data=BA,na.action=na.omit)
 
 #random slopes is best
-M1<-lme(LightM~BAPERCM,random=~BAPERCM|Block,data=BA,na.action=na.omit)
-AICc(M0.2,M1)# the null model is better
+M1<-lmer(LightM~BAPERCM2+(1|Block),data=BA,na.action=na.omit)
+M2<-lmer(LightM~BAPERCM2+I(BAPERCM2^2)+(1|Block),data=BA,na.action=na.omit)
+M3<-lmer(LightM~BAPERCM2+I(BAPERCM2^2)+I(BAPERCM2^3)+(1|Block),data=BA,na.action=na.omit)
 
+AICc(M0.1,M1,M2,M3)# the null model is better
 
-theme_set(theme_bw(base_size=12))
-ggplot(BA,aes(x=BAPERCM*100,y=LightM))+geom_point()+xlab("Percentage change in basal area since 1964")+facet_wrap(~Year2)+ylab("Community weighted mean light dependancy")+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
-setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures/Forest_collapse")
-ggsave("Light_BA_loss.png",width = 12,height = 8,units = "in",dpi = 300)
+modlist<-list(M0,M1,M2,M3)
+
+Mod_sel_light_Tree<-model.sel(modlist)
+Mod_sel_light_Tree
+Mod_sel_light_Tree$R_sq<-c(r.squaredGLMM(M3)[1],r.squaredGLMM(M2)[1],r.squaredGLMM(M1)[1],r.squaredGLMM(M0)[1])
+
+Mod.avg_Light<-model.avg(Mod_sel_light_Tree,subset = delta<7)
+summary(Mod.avg_Light)
+
+write.csv(Mod_sel_light_Tree,"Figures/Mod_sel_light_Tree.csv")
+
 
 #then nitrogen
-M0.1<-lme(NitM~1,random=~1|Block,data=BA,na.action=na.omit)
-M0.2<-lme(NitM~1,random=~BAPERCM|Block,data=BA,na.action=na.omit)
-AICc(M0.1,M0.2)
+BA$NitM2<-qlogis((BA$NitM/max(BA$NitM,na.rm = T))-0.01)
+M0<-lmer(NitM2~1+(1|Block),data=BA,na.action=na.omit)
+
 
 #random slopes is best
-M1<-lme(NitM~BAPERCM,random=~BAPERCM|Block,data=BA,na.action=na.omit)
-M2<-lme(NitM~BAPERCM+Year2,random=~BAPERCM|Block,data=BA,na.action=na.omit)
-M3<-lme(NitM~BAPERCM*Year2,random=~BAPERCM|Block,data=BA,na.action=na.omit)
-AICc(M0.2,M1,M2,M3)# the null model is better
+M1<-lmer(NitM2~BAPERCM2+(1|Block),data=BA,na.action=na.omit)
+M2<-lmer(NitM2~BAPERCM2+I(BAPERCM2^2)+(1|Block),data=BA,na.action=na.omit)
+M3<-lmer(NitM2~BAPERCM2+I(BAPERCM2^2)+I(BAPERCM2^3)+(1|Block),data=BA,na.action=na.omit)
+AICc(M0,M1,M2,M3)# the null model is better
+plot(subset(BA$BAPERCM2,!is.na(BA$LightM)),subset(BA$NitM2,!is.na(BA$LightM)))
+
+length(subset(BA$BAPERCM2,!is.na(BA$LightM)))
 
 
-theme_set(theme_bw(base_size=12))
-ggplot(BA,aes(x=BAPERCM*100,y=NitM))+geom_point()+facet_wrap(~Year2)+xlab("Percentage change in basal area since 1964")+ylab("Community weighted mean nitrogen dependancy")+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
-setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures/Forest_collapse")
-ggsave("Nit_BA_loss.png",width = 12,height = 8,units = "in",dpi = 300)
+modlist<-list(M0,M1,M2,M3)
+
+Mod_sel_Nit_Tree<-model.sel(modlist)
+Mod_sel_Nit_Tree
+Mod_sel_Nit_Tree$R_sq<-c(r.squaredGLMM(M3)[1],r.squaredGLMM(M2)[1],r.squaredGLMM(M1)[1],r.squaredGLMM(M0)[1])
+
+coef(summary(M3))
+
+Mod.avg_Light<-model.avg(Mod_sel_Nit_Tree,subset = delta<7)
+summary(Mod.avg_Light)
+
+plot(M1)
+
+min(BA$BAPERCM2)
+length(subset(BA$BAPERCM2,!is.na(BA$LightM)))
+length(predict(M3,re.form=NA))
+write.csv(Mod_sel_light_Tree,"Figures/Mod_sel_light_Tree.csv")
+
+
 
 #then moisture
-theme_set(theme_bw(base_size=12))
-ggplot(BA,aes(x=BAPERCM*100,y=MoistM))+geom_point()+facet_wrap(~Year2)+xlab("Percentage change in basal area since 1964")+ylab("Community weighted mean moisture dependancy")+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
-setwd("C:/Users/Phil/Dropbox/Work/Active projects/Forest collapse/Denny_collapse/Figures/Forest_collapse")
-ggsave("Moist_BA_loss.png",width = 12,height = 8,units = "in",dpi = 300)
+#first rescale Ellenburg value
+BA$MoistM2<-qlogis((BA$MoistM/max(BA$MoistM,na.rm = T))-0.01)
 
+M0<-lmer(MoistM2~1+(1|Block),data=BA,na.action=na.omit)
+#random slopes is best
+M1<-lmer(MoistM2~BAPERCM2+(1|Block),data=BA,na.action=na.omit)
+M2<-lmer(MoistM2~BAPERCM2+I(BAPERCM2^2)+(1|Block),data=BA,na.action=na.omit)
+M3<-lmer(MoistM2~BAPERCM2+I(BAPERCM2^2)+I(BAPERCM2^3)+(1|Block),data=BA,na.action=na.omit)
+AICc(M0,M1,M2,M3)# the null model is better
+
+plot(BA$BAPERCM2,BA$MoistM)
