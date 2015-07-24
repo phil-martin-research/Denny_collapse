ca34d4693b34371ea545721150576e403b272bf4
Phil Martin <philip.martin.1981@gmail.com>
2015-07-24 11:54:55 +0100

    added script for climate analysis for reviewers

--- Scripts/Climate_analyses.R
+++ Scripts/Climate_analyses.R
@@ -3,45 +3,45 @@ library(ggplot2)
 library(plyr)
 library(reshape2)
 
+#import climate data from Hurn weather station
 Climate<-read.csv("data/Climate_data.csv")
-se <- function(x) sqrt(var(x)/length(x))
-head(Climate)
-
+#produce mean temperature variable by getting mean of max and min temperatures
 Climate<-ddply(Climate,.(Year,Month),mutate,m_temp=(tmax+tmin)/2)
-Climate2<-ddply(Climate,.(Month),mutate,mean_temp=mean(m_temp),sd_temp=sd(m_temp),
-                mean_rain=mean(rain),sd_rain=sd(rain))
+#subset to keep only April - September
+Climate2<-subset(Climate,Month>=4&Month<10)
+#produce mean temps and total rainfall for each year
+Climate3<-ddply(Climate2,.(Year),summarise,mean_temp=mean(m_temp),sum_rain=sum(rain))
+#produce standard deviations for temperature and rainfall
+Temp_SD<-sd(Climate3$mean_temp-mean(Climate3$mean_temp))
+Rain_SD<-sd(Climate3$sum_rain-mean(Climate3$sum_rain))
+#code temperature as extreme if it it >1SD away from mean
+Climate3$Ex_temp<-ifelse(Climate3$mean_temp>mean(Climate3$mean_temp)+Temp_SD,TRUE,FALSE)
+
+#plot climate anomolies
+theme_set(theme_bw(base_size=12))
+Anom_plot1<-ggplot(Climate3,aes(x=Year,y=sum_rain-mean(Climate3$sum_rain),fill=mean_temp-mean(Climate3$mean_temp)))+
+  geom_bar(stat="identity")+scale_fill_gradient2(expression(paste("Deviation from mean \nApril-September temperature(",degree,"C)")),low="blue",mid="grey",high="red")
+Anom_plot2<-Anom_plot1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour="black",fill=NA))     
+Anom_plot2+ylab("Deviation from mean April-September rainfall (mm)")+geom_hline(y=0,lty=1)
+ggsave("Figures/Climate_anomolies.png",width = 8,height=6,units = "in",dpi=800)
 
 
+#produce models of changes in climate
+#first rain
+Rain_M0<-lm(sum_rain~1,data=Climate3)
+Rain_M1<-lm(sum_rain~Year,data=Climate3)
 
-ggplot(Climate2,aes(x=Year,y=m_temp))+geom_point()+facet_wrap(~Month,scales = "free_y")+geom_smooth(method="lm")
-ggplot(Climate2,aes(x=Year,y=rain))+geom_point()+facet_wrap(~Month,scales = "free_y")+geom_smooth(method="lm")
-
-ggplot(Climate2,aes(x=Year,y=rain-mean_rain))+geom_bar(stat="identity")+facet_wrap(~Month,scales = "free_y")
-
-Climate3<-subset(Climate2,Month>=4&Month<10)
-
-Climate4<-ddply(Climate3,.(Year),summarise,mean_temp=mean(m_temp),sd_temp=sd(m_temp),
-                mean_rain=sum(rain))
-
-Climate4$extreme_temp<-ifelse(Climate4$mean_temp>(mean(Climate4$mean_temp)+sd(Climate4$mean_temp)),"Extreme","Not extreme")
-
-sd(Climate4$mean_rain-mean(Climate4$mean_rain))
-
-ggplot(Climate4,aes(x=Year,y=mean_temp))+geom_point()+geom_smooth(method="lm")
-
-ggplot(Climate4,aes(x=Year,y=mean_rain-mean(Climate4$mean_rain),fill=mean_temp-mean(mean_temp)))+geom_bar(stat="identity")+geom_hline(y=86,lty=2)+geom_hline(y=-86,lty=2)+scale_fill_gradient2(low="blue",mid="grey",high="red")
+#now temperature
+Temp_M0<-lm(mean_temp~1,data=Climate3)
+Temp_M1<-lm(mean_temp~Year,data=Climate3)
+par(mfrow=c(2,2))
+plot(Temp_M1)
 
 
-Climate4$Year2<-cut(Climate4$Year, c(0,1960,1970,1980,1990,2000,2014),include.lowest=F,labels =c("1960s","1970s","1980s","1990s","2000s","2010s"))
-
-#generate minimum convex hull
-find_hull <- function(Climate4) Climate4[chull(Climate4$mean_rain, Climate4$mean_temp), ]
-hulls <- ddply(Climate4, "Year2", find_hull)
-
-
-ggplot(Climate4,aes(x=mean_rain,y=mean_temp,colour=Year2,fill=NULL))+geom_point()+
-  geom_polygon(data=hulls,alpha=0.5,fill=NA)
-
-Climate5<-ddply(Climate4,.(Year2),summarise,temp_sum=mean(mean_temp),se_temp=se(mean_temp),rain_sum=mean(mean_rain),se_rain=se(mean_rain))
-ggplot(Climate5,aes(x=rain_sum,y=temp_sum,colour=Year2,fill=NULL))+geom_point(size=3)+
-  geom_errorbar(aes(ymin = temp_sum+(2*se_temp),ymax = temp_sum-(2*se_temp)))+geom_errorbarh(aes(xmin = rain_sum+(2*se_rain),xmax = rain_sum-(2*se_rain)))
+Climate3$temp_pred<-predict(Temp_M1)
+#plot these results
+theme_set(theme_bw(base_size=12))
+Temp_plot1<-ggplot(Climate3,aes(x=Year,y=mean_temp))+geom_point(size=3,shape=1)+geom_line(lty=2,size=0.5,colour="grey",alpha=0.5)+geom_line(data=Climate3,aes(y=temp_pred),size=2)
+Temp_plot2<-Temp_plot1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour="black",fill=NA))     
+Temp_plot2+ylab(expression(paste("Mean annual April-September temperature(",degree,"C)")))
+ggsave("Figures/Temperature_change.png",width = 8,height=6,units = "in",dpi=800)
